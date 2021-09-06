5e2cab840376eba9a5eb2e9225ebb9ea11efe4aa
Minji Jung <52596617+mjjunng@users.noreply.github.com>
2021-09-07 03:30:01 +0900

    Update WebSecurityConfig.java

--- src/main/java/coview/coview/WebSecurityConfig.java
+++ src/main/java/coview/coview/WebSecurityConfig.java
@@ -26,21 +26,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception { // 5
         http
                 .authorizeRequests()
-                .antMatchers("/", "/login", "/create_account", "/h2-console/**").permitAll() // 누구나 접근 허용
-                .antMatchers("/dashboard").hasRole("USER") // USER만 접근 가능
-                .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
+                .antMatchers("/", "/login", "/create_account", "/h2-console/**").permitAll() 
+                .antMatchers("/dashboard").hasRole("USER") 
+                .anyRequest().authenticated() 
                 .and()
                 .csrf().ignoringAntMatchers("/h2-console/**")
                 .and()
                 .headers().frameOptions().disable()
                 .and()
                 .formLogin()
-                .loginPage("/login") // 로그인 페이지 링크
-                .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
                 .and()
                 .logout()
-                .logoutSuccessUrl("/") // 로그아웃 성공시 리다이렉트 주소
-                .invalidateHttpSession(true) // 세션 날리기
+                .logoutSuccessUrl("/") 
+                .invalidateHttpSession(true)
         ;
     }
 
