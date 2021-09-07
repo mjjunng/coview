d2eb2b6e464c4ff7ef6950003e7fd61f13f15e8b
Minji Jung <mjjunghp@naver.com>
2021-09-08 02:33:25 +0900

    dev-show meetingList, invite func

--- src/main/java/coview/coview/WebSecurityConfig.java
+++ src/main/java/coview/coview/WebSecurityConfig.java
@@ -28,19 +28,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/login", "/create_account", "/h2-console/**").permitAll() // 누구나 접근 허용
                 .antMatchers("/dashboard", "/coview").authenticated() // 로그인해야 접근가능
-                .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
+                .anyRequest().authenticated()
                 .and()
                 .csrf().ignoringAntMatchers("/h2-console/**")
                 .and()
                 .headers().frameOptions().disable()
                 .and()
                 .formLogin()
-                .loginPage("/login") // 로그인 페이지 링크
-                .defaultSuccessUrl("/dashboard") // 로그인 성공 후 리다이렉트 주소
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
                 .and()
                 .logout()
-                .logoutSuccessUrl("/") // 로그아웃 성공시 리다이렉트 주소
-                .invalidateHttpSession(true) // 세션 날리기
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
         ;
     }
 
